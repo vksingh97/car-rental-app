1a7c4d7cf7582bb04a562aef786dc5b6c77bd798
Vivek <viveksingh.singh50@gmail.com>
2022-02-04 12:05:23 +0530

    improved indentation

--- src/components/RentalForm.js
+++ src/components/RentalForm.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import './RentalForms.css'
-import TextField from '@mui/material/TextField'
-import MenuItem from '@mui/material/MenuItem';
+import "./RentalForms.css";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
 
-const carTypes = [
-  'Hatchback', 'Sedan', 'SUV'
-]
+const carTypes = ["Hatchback", "Sedan", "SUV"];
 
 const RentalForm = () => {
   return (
@@ -35,7 +33,7 @@ const RentalForm = () => {
                 <input className="form-control" autoComplete="off" />
               </div>
             </div> */}
-            
+
             {/* 
                   <div className="col-5 inputBox1">
                     <input type="email" name="email" required></input>
@@ -49,58 +47,61 @@ const RentalForm = () => {
                   <input type="submit" name="sign-in" value="Sign In" />
                 
               </div> */}
-<div className="col-12 box">
-<div>
-              <TextField
-              className="col-5"
-              required
-        id="source_location_field"
-        label="Source Location"
-        style={{marginRight: 55}}
-        // value={name}
-        // onChange={handleChange}
-      />
-              <TextField
-              className="col-5"
-              required
-        id="destination_location_field"
-        label="Destination"
-        // value={name}
-        // onChange={handleChange}
-      />
-      </div>
-      <div className="col-12">
-      <TextField className="col-11"
-          id="car_type_field"
-          required
-          select
-          label="Enter Car Type"
-          style={{marginTop:50}}
-          // value={currency}
-          // onChange={handleChange}
-        >
-          {carTypes.map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
+            <div className="col-12 box">
+              <div>
+                <TextField
+                  className="col-5"
+                  required
+                  id="source_location_field"
+                  label="Source Location"
+                  style={{ marginRight: 55 }}
+                  // value={name}
+                  // onChange={handleChange}
+                />
+                <TextField
+                  className="col-5"
+                  required
+                  id="destination_location_field"
+                  label="Destination"
+                  // value={name}
+                  // onChange={handleChange}
+                />
+              </div>
+              <div className="col-12">
+                <TextField
+                  className="col-11"
+                  id="car_type_field"
+                  required
+                  select
+                  label="Enter Car Type"
+                  style={{ marginTop: 50 }}
+                  // value={currency}
+                  // onChange={handleChange}
+                >
+                  {carTypes.map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </div>
 
-      <div className="col-12">
-      <TextField className="col-11"
-          id="travellers_count"
-          label="Number of Travellers"
-          style={{marginTop:50, marginBottom:50}}
-          // value={currency}
-          // onChange={handleChange}
-        />
-      </div>
-      <div className="col-12">
-      <button className="btn btn-primary col-10 bid-button">Enter Bid Details</button>
-      </div>
-
-      </div>
+              <div className="col-12">
+                <TextField
+                  className="col-11"
+                  id="travellers_count"
+                  label="Number of Travellers"
+                  style={{ marginTop: 50, marginBottom: 50 }}
+                  // value={currency}
+                  // onChange={handleChange}
+                />
+              </div>
+              <div className="col-12">
+                <button className="btn btn-primary col-10 bid-button">
+                  Enter Bid Details
+                </button>
+              </div>
+            </div>
           </Form>
         </div>
       )}
